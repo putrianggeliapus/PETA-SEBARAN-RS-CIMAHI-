ac9849f2c913cfd7c7ef5df9d7c8d078dae6550b
putrianggeliapus <putrianggeliapuspa@upi.edu>
2024-10-18 00:16:50 +0700

    Add files via upload

---
+++ styles/2RSSSCIMMMyPlaces_5_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_2RSSSCIMMMyPlaces_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    ''
+    var labelText = "";
+    size = 0;
+    var labelFont = "11.700000000000001px \'Segoe UI Black\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if (feature.get("Name") !== null) {
+        labelText = String(feature.get("Name"));
+    }
+    
+    var style = [ new ol.style.Style({
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor, bufferWidth)
+    })];;
+
+    return style;
+};
